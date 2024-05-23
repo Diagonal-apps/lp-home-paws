76fe8c2a22a6c93e9db4cdbcf060c953cc98ee57
GABRIEL SANTOS <53496280+ccolass@users.noreply.github.com>
2024-05-22 22:02:42 -0300

    tag google
    arquivo gtml.js criado e laout.js modificado

---
+++ src/app/gtm.js
@@ -0,0 +1,35 @@
+'use client';
+
+import { useEffect } from 'react';
+
+const GTM_ID = 'GTM-KSN29HSH';
+
+export default function GoogleTagManager() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.innerHTML = `
+      (function(w,d,s,l,i){
+        w[l]=w[l]||[];
+        w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+        var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),
+            dl=l!='dataLayer'?'&l='+l:'';
+        j.async=true;
+        j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+        f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','${GTM_ID}');
+    `;
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <noscript>
+      <iframe src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+              height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
+    </noscript>
+  );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 
 import Head from 'next/head'
+import GoogleTagManager from './gtm';
 import './globals.css'
-
 
 export const metadata = {
   title: 'Home Paws',
@@ -25,8 +25,12 @@ export default function RootLayout({ children }) {
         <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css" />
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+        
       </Head>
-      <body>{children}</body>
+      <body>
+        <GoogleTagManager />
+        {children}
+      </body>
     </html>
   )
 }
